c7f683ddd598b6e861ee6e3e8275b2eeece40ac2
Anper <mail@s3f.ru>
2018-08-13 20:42:58 +0300

    init project, type helloworld

---
+++ main.c
@@ -0,0 +1,15 @@
+
+char hello_string[] = "ARM1 forever";
+void* tty = (void*)0x0000ff00;
+
+static int result = 0;
+
+void main() {
+    int a = 5;
+
+    for(int i = 0; i < 5; i++) {
+    	a = a + 1;
+    }
+
+    result = a;
+}
---
+++ startup.s
@@ -0,0 +1,49 @@
+.section .irq
+b start // Reset
+b error_handler // Undefined_Instruction_Trap
+b error_handler // Software Interrupt
+b error_handler // Abort (Prefetch)
+b error_handler // Abort (Data)
+b error_handler // Address Exception
+b error_handler // Normal Interrupt (IRQ)
+b error_handler // Fast Interrupt (FIRQ)
+
+.text
+
+.global start
+start:
+mov     r2, #12
+/*; movs    pc, r2*/
+
+ldr sp, estack
+ldr fp, estack
+/* bl foo */
+mov r7, #10
+b test_s
+
+estack: .word _estack
+
+.global test_s
+test_s:
+mov r2, #33
+ldr r5, tty_ptr
+ldr r3, [r5]
+ldr r4, hello_string_ptr
+loop:
+nop
+nop
+nop
+ldr r2, [r4]
+and r2, r2, #0xff
+str r2, [r3]
+add r4, r4, #1
+b loop
+
+error_handler:
+nop
+nop
+nop
+b error_handler
+
+tty_ptr: .word tty
+hello_string_ptr: .word hello_string
