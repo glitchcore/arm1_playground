329f3a36165598c14bad3b2c972f09e899b2d78d
Anper <mail@s3f.ru>
2018-08-14 01:48:11 +0300

    print from c-code

--- main.c
+++ main.c
@@ -1,15 +1,21 @@
-
-char hello_string[] = "ARM1 forever";
-void* tty = (void*)0x0000ff00;
+char* tty = (void*)0x0000ff00;
 
 static int result = 0;
+
+void print(char* str) {
+    while(*str) {
+        *tty = *str++;
+    }
+}
 
 void main() {
     int a = 5;
 
-    for(int i = 0; i < 5; i++) {
-    	a = a + 1;
+    for(int i = 0; i < 2; i++) {
+        a = a + 1;
+        print("ARM1 forever!");
+        print(" ");
     }
 
-    result = a + 1;
+    result = a << 1;
 }
--- startup.s
+++ startup.s
@@ -17,33 +17,18 @@ mov     r2, #12
 
 ldr sp, estack
 ldr fp, estack
+mov r7, #0
 bl main
 mov r7, #0xFE00
-b test_s
-
-estack: .word _estack
-
-.global test_s
-test_s:
-mov r2, #33
-ldr r5, tty_ptr
-ldr r3, [r5]
-ldr r4, hello_string_ptr
 loop:
 nop
 nop
-nop
-ldr r2, [r4]
-and r2, r2, #0xff
-str r2, [r3]
-add r4, r4, #1
 b loop
+
+estack: .word _estack
 
 error_handler:
 nop
 nop
 nop
 b error_handler
-
-tty_ptr: .word tty
-hello_string_ptr: .word hello_string
