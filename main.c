58a326b98e4aace98581dc19003cb2be118003c7
Anper <mail@s3f.ru>
2018-08-14 20:23:11 +0300

    write to framebuffer

--- main.c
+++ main.c
@@ -11,8 +11,23 @@ void print(char* str) {
 void main() {
     int a = 5;
 
+    int* frame = (int*)tty + 4;
+
+    *frame = (1 << 24) | (10 << 16) | (10 << 8);
+    int* beginLine = frame;
+
+    frame++;
+
+    *frame++ = (2 << 24) | (3 << 28) | (20 << 16) | (20 << 8);
+    *frame = 0;
+
+    for(char x = 10; x < 30; x++) {
+        *beginLine = (1 << 24) | (x << 16) | (10 << 8);
+        *frame = 0;
+    }
+
     for(int i = 0; i < 2; i++) {
-        a = a + 1;
+        a = a + (a << 2);
         print("ARM1 forever!");
         print(" ");
     }
