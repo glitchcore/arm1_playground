76041e5f1ad996598501815b33d932b51f275d16
Anper <mail@s3f.ru>
2018-08-25 01:07:58 +0300

    refactor c code

--- main.c
+++ main.c
@@ -1,6 +1,28 @@
-char* tty = (void*)0x0000ff00;
+#include <stdint.h>
 
-static int result = 0;
+uint32_t* tty = (uint32_t*)0x0000ff00;
+
+typedef enum _Color {
+    MOVE = 1,
+    LINE = 2,
+    CIRCLE = 3
+} Color;
+
+typedef enum _Type {
+    BLACK = 1,
+    WHITE = 2,
+    RED = 3,
+    GREEN = 4,
+    BLUE = 5
+} Type;
+
+typedef struct _Part {
+    uint8_t x;
+    uint8_t y;
+    uint8_t r;
+    Color color;
+    Type type;
+} Part;
 
 void print(char* str) {
     while(*str) {
@@ -8,36 +30,45 @@ void print(char* str) {
     }
 }
 
-void main() {
-    int a = 5;
+void update_part(Part* part, uint32_t* frame_ptr) {
+    *frame_ptr = (part->type << 24) | (part->color << 28) |
+        (part->x << 16) | (part->y << 8) | part->r;
+}
 
-    volatile int* frame = (int*)tty + 4;
+volatile uint32_t* frame = 0;
+uint32_t* place_part(Part* part) {
+    uint32_t* frame_ptr = frame++;
+    update_part(part, frame_ptr);
 
-    *frame = (1 << 24) | (10 << 16) | (10 << 8);
-    volatile int* beginLine = frame;
+    return frame_ptr;
+}
 
-    frame++;
-
-    *frame++ = (2 << 24) | (3 << 28) | (60 << 16) | (100 << 8);
+void render() {
     *frame = 0;
+}
+
+void main() {
+    frame = tty + 4;
+
+    Part begin_line_part = { .type = MOVE, .x = 10, .y = 10 };
+    Part end_line_part = {.type = LINE, .color = RED, .x = 60, .y = 100};
+
+    uint32_t* begin_line = place_part(&begin_line_part);
+
+    place_part(&end_line_part);
+    render();
 
     while(1) {
         for(char x = 10; x < 120; x += 3) {
-            *beginLine = (1 << 24) | (x << 16) | (10 << 8);
-            *frame = 0;
+            begin_line_part.x = x;
+            update_part(&begin_line_part, begin_line);
+            render();
         }
 
         for(char x = 120; x > 10; x -= 3) {
-            *beginLine = (1 << 24) | (x << 16) | (10 << 8);
-            *frame = 0;
+            begin_line_part.x = x;
+            update_part(&begin_line_part, begin_line);
+            render();
         }
     }
-
-    for(int i = 0; i < 2; i++) {
-        a = a + (a << 2);
-        print("ARM1 forever!");
-        print(" ");
-    }
-
-    result = a << 1;
 }
